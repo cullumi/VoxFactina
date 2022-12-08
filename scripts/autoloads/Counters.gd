02c53eaec47155732533e43eb524bede11b78259
Ian Cullum <cullumi@wwu.edu>
2022-12-08 07:29:06 -0800

    Update to Counter

--- scripts/autoloads/Counters.gd
+++ scripts/autoloads/Counters.gd
@@ -10,12 +10,12 @@ func makes(keys:Array, count:Dictionary=counters):
 func make(key, count:Dictionary=counters):
 	count[key] = count.get(key, 0)
 
-func push(key, amount=1, count:Dictionary=counters):
+func push(key, amount:int=1, count:Dictionary=counters):
 	count[key] = count.get(key, 0) + amount
 
-func increment(key, amount=1, count:Dictionary=counters):
+func increment(key, amount:int=1, count:Dictionary=counters):
 	count[key] += int(amount)
-func decrement(key, amount=1, count:Dictionary=counters):
+func decrement(key, amount:int=1, count:Dictionary=counters):
 	count[key] -= int(amount)
 	
 func peek(key, do_print:bool=false, count:Dictionary=counters) -> int:
@@ -24,8 +24,10 @@ func peek(key, do_print:bool=false, count:Dictionary=counters) -> int:
 	return count[key]
 func peek_all(do_print:bool=false, keys=null, count:Dictionary=counters):
 	if keys == null: keys = count.keys()
+	var res:Array = []
 	for key in keys:
-		peek(key, do_print, count)
+		res.append(peek(key, do_print, count))
+	return res
 
 func pop(key, do_print:bool=false, count:Dictionary=counters):
 	if do_print:
@@ -33,7 +35,11 @@ func pop(key, do_print:bool=false, count:Dictionary=counters):
 	var res = count[key]
 	count[key] = 0
 	return res
-func pop_all(do_print:bool=false, keys=null, count:Dictionary=counters):
+	
+func pop_all(erase:bool=false, do_print:bool=false, keys=null, count:Dictionary=counters):
 	if keys == null: keys = count.keys()
+	var res:Array = []
 	for key in keys:
-		pop(key, do_print, count)
+		res.append(pop(key, do_print, count))
+	if erase: count.clear()
+	return res
