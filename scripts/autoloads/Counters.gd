c4c6093000c0e1f08076be747e685cb15364c5f0
Ian Cullum <cullumi@wwu.edu>
2022-12-08 02:04:58 -0800

    Optional Counter Thread Safety + Count.Push()
    + Count class functions now accept optional dictionary for counter storage

--- scripts/autoloads/Counters.gd
+++ scripts/autoloads/Counters.gd
@@ -3,29 +3,37 @@ extends Node
 # Named "Count" in Autoload
 
 var counters = {}
-func makes(keys:Array):
+
+func makes(keys:Array, count:Dictionary=counters):
 	for key in keys:
 		make(key)
-func make(key):
-	if not counters.has(key):
-		counters[key] = 0
-func increment(key, amount=1):
-	counters[key] += int(amount)
-func decrement(key, amount=1):
-	counters[key] -= int(amount)
-func peek(key, do_print:bool=false) -> int:
+func make(key, count:Dictionary=counters):
+	count[key] = count.get(key, 0)
+
+func push(key, amount=1, count:Dictionary=counters):
+	count[key] = count.get(key, 0) + amount
+
+func increment(key, amount=1, count:Dictionary=counters):
+	count[key] += int(amount)
+func decrement(key, amount=1, count:Dictionary=counters):
+	count[key] -= int(amount)
+	
+func peek(key, do_print:bool=false, count:Dictionary=counters) -> int:
 	if do_print:
-		print(key, ": ", counters[key])
-	return counters[key]
-func peek_all():
-	for key in counters.keys():
-		peek(key)
-func pop(key, do_print:bool=false):
-	if do_print:
-		print(key, ": ", counters[key])
-	var res = counters[key]
-	counters[key] = 0
-	return res
-func pop_all(do_print:bool=false, keys:Array=counters.keys()):
+		print(key, ": ", count[key])
+	return count[key]
+func peek_all(do_print:bool=false, keys=null, count:Dictionary=counters):
+	if keys == null: keys = count.keys()
 	for key in keys:
-		pop(key, do_print)
+		peek(key, do_print, count)
+
+func pop(key, do_print:bool=false, count:Dictionary=counters):
+	if do_print:
+		print(key, ": ", count[key])
+	var res = count[key]
+	count[key] = 0
+	return res
+func pop_all(do_print:bool=false, keys=null, count:Dictionary=counters):
+	if keys == null: keys = count.keys()
+	for key in keys:
+		pop(key, do_print, count)
